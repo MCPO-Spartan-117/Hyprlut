6bb3ce05ee1364de1dcc979510ee96d73ec07672
Мастер Чиф <43174917+MCPO-Spartan-117@users.noreply.github.com>
2025-08-15 01:20:50 -0400

    Remove static from border

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ inline SP<CTexture> loadAsset(const std::string& path) {
     tex->setTexParameter(GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     tex->setTexParameter(GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
-    static const float border[] = { 1.0, 0.0, 1.0, 1.0 };
+    const float border[] = { 1.0, 0.0, 1.0, 1.0 };
     tex->setTexParameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
     tex->setTexParameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
     glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, border);
